307322dbc1d31d0957e3486a85c9206c4850737f
Matt Witkowski <taztingo503@gmail.com>
2024-07-24 01:18:06 -0400

    Create logging module basics.

---
+++ logging/go.mod
@@ -0,0 +1,3 @@
+module github.com/taztingo/go-practice/logging
+
+go 1.21.5
---
+++ logging/logger.go
@@ -0,0 +1,12 @@
+package logging
+
+// Root logger functions
+// Error(msg)
+// Warning(msg)
+// Info(msg)
+// Debug(msg)
+// Logger(name)
+
+// Handlers
+// Filters
+// Formatters
